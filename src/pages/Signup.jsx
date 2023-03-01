bc287a278902822374f73bafc84b726da1476edc
Maik Schmidt <maikschmidt91@gmail.com>
2023-03-01 23:52:35 +0100

    Quick code tidy up

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,28 +3,21 @@ import React from 'react'
 function Signup() {
   return (
     <div>
-      <h1> SignUp
-      </h1>
+      <h1>SignUp</h1>
       <form >
-       <label>
-UserName:
-       <input type="text" placeholder="Name" />
+       <label>UserName:
+        <input type="text" placeholder="Name" />
        </label>
-       <label>
-Email Address:
-       <input type="text" placeholder="Email" />
+       <label>Email Address:
+        <input type="text" placeholder="Email" />
        </label>
-       <label>
-Password:
-       <input type="text" placeholder="password" />
+       <label>Password:
+        <input type="text" placeholder="password" />
        </label>
-       <label>
-Repeat Password:
-       <input type="text" placeholder="repeat" />
+       <label>Repeat Password:
+        <input type="text" placeholder="repeat" />
        </label>
-<button tvpe= 'submit'>
-   Create Account 
-</button>
+       <button tvpe= 'submit'>Create Account</button>
       </form>
     </div>
   )
