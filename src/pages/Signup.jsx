cd43a787d4bb8e8fd37326270e0e3cc5ca7166d7
asmaaelhadad <asmaa.elhadad@gmail.com>
2023-03-01 18:00:48 +0100

    first Day

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+      <h1> SignUp
+      </h1>
+      <form >
+       <label>
+UserName:
+       <input type="text" placeholder="Name" />
+       </label>
+       <label>
+Email Address:
+       <input type="text" placeholder="Email" />
+       </label>
+       <label>
+Password:
+       <input type="text" placeholder="password" />
+       </label>
+       <label>
+Repeat Password:
+       <input type="text" placeholder="repeat" />
+       </label>
+<button tvpe= 'submit'>
+   Create Account 
+</button>
+      </form>
+    </div>
+  )
+}
+
+export default Signup
