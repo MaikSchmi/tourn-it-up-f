d49591dc71edda22642ec12a96f8fe6639f490c8
Maik Schmidt <maikschmidt91@gmail.com>
2023-03-02 15:00:44 +0100

    Styling and code cleanup

--- src/contexts/Auth.context.jsx
+++ src/contexts/Auth.context.jsx
@@ -7,16 +7,19 @@ const [isAuthenticated , setIsAuthenticated ] = useState (false)
 const [isLoading , setIsLoading ] = useState (true)
 const [user , setUser ] = useState ({})
 const storeToken = (token ) => {
-    localStorage.setItem ("token" , token)}
-const authenticateUser = async () => {
-const storageToken = localStorage.getItem('token')
-if (storageToken) {
-try{
-const response = await axios.get('http://localhost:5005/auth/verify')
-   const userDetails = response.data
-  console.log (userDetails)
-}catch(err){console.log(err)}
+    localStorage.setItem ("token" , token)
 }
+const authenticateUser = async () => {
+    const storageToken = localStorage.getItem('token')
+    if (storageToken) {
+        try{
+            const response = await axios.get('http://localhost:5005/auth/verify')
+            const userDetails = response.data
+            console.log ("HALLO: ", userDetails)
+        } catch(err) {
+            console.log(err)
+        }
+    }
 }
     useEffect (()=> {
  authenticateUser() 
