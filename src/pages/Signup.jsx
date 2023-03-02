8408d0d727e02e79bddeec60f715c569c2c85521
asmaaelhadad <asmaa.elhadad@gmail.com>
2023-03-02 11:37:50 +0100

    signup done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 function Signup() {
+  const [username , setUsername ] = useState ('')
+  const [password , setPassword ] = useState ('')
+  const [email , setEmail ] = useState ('')
+  const [repeatPassword , setRepeatPassword ] = useState ('')
+   const navigate = useNavigate()
+ 
+  const handleUserCreate = async (event) => {
+       event.preventDefault()
+       if (password === repeatPassword){
+    try{ 
+      const user = await axios.post('http://localhost:5005/auth/signup', {
+        username: username,
+        password: password,
+        email: email
+      })
+console.log(user)
+navigate('/login')
+    }
+  catch (error){console.log(error)}
+  }
+    else {console.log("passwords don't match")}
+} 
   return (
     <div>
       <h1>SignUp</h1>
-      <form >
-       <label>UserName:
-        <input type="text" placeholder="Name" />
+      <form onSubmit={handleUserCreate }>
+       <label>UserName: 
+        <input type="text" placeholder="Name" value = {username} onChange = {(event)=> { setUsername(event.target.value)}} />
        </label>
        <label>Email Address:
-        <input type="text" placeholder="Email" />
+        <input type="email" placeholder="Email" value = {email} onChange = {(event)=> { setEmail(event.target.value)}} />
        </label>
        <label>Password:
-        <input type="text" placeholder="password" />
+        <input type="password" placeholder="password" value = {password} onChange = {(event)=> { setPassword(event.target.value)}} />
        </label>
        <label>Repeat Password:
-        <input type="text" placeholder="repeat" />
+        <input type="password" placeholder="repeat"  value = {repeatPassword} onChange = {(event)=> { setRepeatPassword(event.target.value)}} />
        </label>
-       <button tvpe= 'submit'>Create Account</button>
+       <button type= 'submit'>Create Account</button>
       </form>
     </div>
   )
